7e34c41c834789e17cbe6693f89f9752d632a513
Катя <yrm.katiy@gmail.com>
2025-06-06 16:20:53 +0600

    Add home route to show_routes

--- transport_company/urls.py
+++ transport_company/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from transport.views import show_routes  # <-- добавь в начало
 
 urlpatterns = [
+    path('', show_routes, name='home'),  # <-- добавь это
     path('admin/', admin.site.urls),
     path('transport/', include('transport.urls')),  # явное разделение
     path('routecalc/', include('routecalc.urls')),

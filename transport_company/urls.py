09a5812f6157655643f043451255fb106f582703
Катя <yrm.katiy@gmail.com>
2025-06-07 18:33:26 +0600

    удаляю

--- transport_company/urls.py
+++ transport_company/urls.py
@@ -2,14 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from transport.views import show_routes  # <-- добавь в начало
 
-
 urlpatterns = [
     path('', show_routes, name='home'),  # <-- добавь это
     path('admin/', admin.site.urls),
     path('transport/', include('transport.urls')),  # явное разделение
     path('routecalc/', include('routecalc.urls')),
-
-
 ]
 
 admin.site.site_header = "Панель администратора"
